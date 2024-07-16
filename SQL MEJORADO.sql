554743c063f921d51bd189b53d7683008a8ecea2
jesandyb15 <106169186+jesandyb15@users.noreply.github.com>
2024-07-15 23:47:03 -0400

    Add files via upload

---
+++ SQL MEJORADO.sql
@@ -0,0 +1,113 @@
+Drop schema IF exists ECOMODA;
+create schema ECOMODA;
+USE ECOMODA;
+
+drop table if exists materiales;
+create table materiales(
+id_material int primary key auto_increment not null,
+descripcion varchar(45) not null unique,
+tipo ENUM("hilo", "tela"),
+cantidad int not null,
+precio Decimal(10,2) not null
+);
+
+Select * from materiales;
+Drop table if exists proveedores;
+create table proveedores(
+nombre_proveedor varchar(25) not null,
+id_material int not null,
+descripcion_material varchar(45) not null references materiales(descripcion),
+direccion varchar(45) not null,
+telefono int not null,
+Primary Key (nombre_proveedor),
+FOREIGN KEY (id_material) REFERENCES materiales(id_material),
+foreign key (descripcion_material) references materiales(descripcion)
+);
+select * from proveedores;
+
+Drop table if exists orden_compra;
+create table orden_compra(
+id_orden_compra int auto_increment not null UNIQUE,
+nombre_proveedor varchar(25) not null,
+id_material int not null unique,
+cantidad int not null,
+precio double not null unique,
+Primary key (id_orden_compra),
+foreign key (nombre_proveedor) references proveedores(nombre_proveedor),
+foreign key(id_material) references materiales(id_material)
+);
+
+SELECT * FROM orden_compra;
+
+Drop table if exists productos;
+create table productos(
+id_producto int primary key auto_increment not null,
+descripcion varchar(45) not null unique,
+cantidad int not null,
+precio_unidad double not null
+);
+Select * from productos;
+
+use ecomoda;
+Drop table if exists clientes;
+create table clientes(
+nombre_cliente varchar(45) primary key not null unique,
+id_producto int not null,
+descripcion_producto varchar(45) not null ,
+direccion varchar(45) not null unique,
+telefono int not null,
+foreign key (id_producto) references productos(id_producto),
+foreign key (descripcion_producto) references productos(descripcion)
+);
+Select * from clientes;
+
+Drop table if exists orden_venta;
+create table orden_venta(
+id_orden_venta int auto_increment not null unique,
+nombre_cliente varchar(45) not null,
+id_producto int not null unique,
+descripcion_producto varchar(45) not null ,
+cantidad int not null unique,
+precio double not null unique,
+primary key (id_orden_venta),
+foreign key (id_producto) references productos(id_producto),
+foreign key (nombre_cliente) references clientes(nombre_cliente),
+foreign key (descripcion_producto) references productos(descripcion)
+);
+select * from orden_venta;
+
+
+drop table if exists cuentas_cobrar;
+create table cuentas_cobrar(
+id_cuenta_cobrar int auto_increment primary key not null,
+documento_referencia int not null ,
+estado enum('por cobrar', 'saldada'),
+valor double not null,
+foreign key fk_documento_referencia_cobrar (documento_referencia) references orden_venta(id_orden_venta),
+foreign key fk_valor_cobrar (valor) references orden_venta(precio)
+);
+select *from cuentas_cobrar;
+
+drop table if exists cuentas_pagar;
+create table cuentas_pagar(
+id_cuentas_pagar int auto_increment primary key not null, 
+documento_referencia int not null ,
+estado enum('por pagar', 'saldada'),
+valor double not null,
+material int not null ,
+foreign key fk_documento_referencia (documento_referencia) references orden_compra(id_orden_compra),
+foreign key fk_valor (valor) references orden_compra(precio)
+);
+select * from cuentas_pagar;
+
+alter table cuentas_pagar
+drop column material;
+
+
+
+
+
+
+
+
+
